78e724155b17e3f9f113cfe9869ae6577dac0706
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2019-02-14 01:20:10 +0300

    Practise 1 finished

--- Practise 1/casting.c
+++ Practise 1/casting.c
@@ -1,16 +1,35 @@
+/*
+ * Francisco Javier Blázquez Martínez ~ frblazqu@ucm.es
+ *
+ * Double degree in Mathematics-Computer engineering
+ * Complutense University, Madrid.
+ *
+ * Description: Introduction to types cast. Examples.
+ */
 
-int x;
-short y;
-char z;
-float f1;
+/*
+ * This file is for visualizing how and where (considering the link file)
+ * the variables are stored and how do the instructions translate from c
+ * code to assembly. We are checking the correctness of the operations.
+ */
+
+int x;		// 4 Bytes - Global variable 0x0C000000
+short y;	// 2 Bytes - Global variable 0x0C000006
+char z;     // 1 Bytes - Global variable 0x0C000004
+float f1;	// 4 Bytes - Global variable 0x0C000008
 
 int main() {
+
   // casting
-    x = 234; 
-    y = x; 
-    z = x; // Comprueba su valor en el visor de variables
-    f1 = (float) x; 
-    f1 = 3.4;
-    x = (int) f1; 
+    x = 234;			//Ox000000EA - 234
+    y = x;				//0x00EA	 - 234
+    z = x; 				//0xEA		 - 11101010
+
+    f1 = (float) x;		//0x436A0000 - 234
+    f1 = 3.4;			//0x40599994 ~ 3.4
+    x = (int) f1;		//0x00000003 - 3	Perdemos precisión
+
     return 0;
 }
+
+
--- Practise 1/tipos-estru.c
+++ Practise 1/tipos-estru.c
@@ -1,11 +1,24 @@
-// Arrays, struct, cadenas de caracteres, punteros, operaciones sobre bits
+/*
+ * Francisco Javier Blázquez Martínez ~ frblazqu@ucm.es
+ *
+ * Double degree in Mathematics-Computer engineering
+ * Complutense University, Madrid.
+ *
+ * Description: Introduction to memory location of complex types.
+ */
 
+/*
+ * This file is for visualizing how and where (considering the link file)
+ * the variables are stored and how do the instructions translate from c
+ * code to assembly. We are checking the correctness of the operations.
+ */
 struct t_dummy {
 	char cx;
 	char cy;
 	int lat;
 	int longitud;
 };
+
 struct t_dummy st;
 
 unsigned int tabla[8]= { 0x1, 0x3, 0x5, 0x7, 0x9, 0xB, 0xD, 0xF };
@@ -15,21 +28,29 @@ unsigned int tabla[8]= { 0x1, 0x3, 0x5, 0x7, 0x9, 0xB, 0xD, 0xF };
 #define LED2 0x02
 static unsigned int status = 0;
 // Punteros
-
 unsigned int * puntero;
-char str_tecla[27] = "\nTecla pulsada\0";;
+char str_tecla[27] = "\nTecla pulsada\0";	// 0x0c000020 - 28 Bytes
 char * puntero2;
+
+/*
+ * tabla      ~ 0x0C000000 ~ 8x4 Bytes
+ * std_tecla  ~ 0x0C000020 ~ 28  Bytes
+ * puntero2   ~ 0x0C000040 ~ 1   Byte
+ * puntero    ~ 0x0C000044 ~ 1   Byte
+ * st		  ~ 0x0C000048 ~ 12  Bytes
+ * status     ~ 0x0C00003C ~ 1   Byte
+ */
 
 int main() {
 
-	st.cx = 1;
-	st.cy = 2;
-	st.lat = 3;
-	st.longitud = 4;
+	st.cx = 1;			// Ver 0x0C000048
+	st.cy = 2;			// Ver 0x0C000049
+	st.lat = 3;			// Ver 0x0C00004C
+	st.longitud = 4;	// Ver 0x0C000050
 
-	status |=  LED1;
-	status &=  ~LED1;
-	status ^= (LED1 | LED2);
+	status |=  LED1;			// status = status 0R  LED1
+	status &=  ~LED1;			// status = status AND NOT(LED1)
+	status ^= (LED1 | LED2);	// status = status XOR (LED1 0R LED2)
 	status = (tabla[7]<< 4 ) | tabla [2] | (tabla[4]<<8);
 	status  = (status & ~(0xf << 12)) | (0x7 << 12);
 // punteros y cadenas de caracteres
