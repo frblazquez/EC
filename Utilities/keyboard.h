7140e961db8b97c50a89e7b79a09fb4defbc6f4c
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2019-03-19 13:01:17 +0100

    Components functionallities added

---
+++ Utilities/keyboard.h
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------
+**
+**	  Francisco Javier Blázquez Martínez  ~  frblazqu@ucm.es 
+**      
+**	  Doble grado en Ingeniería informática - Matemáticas
+**	  Universidad Complutense de Madrid. Curso 2018/19
+**
+**    Estructura de Computadores. Facultad de Informática.
+**
+**    Descripción: 
+**	  Interfaz de uso del controlador del teclado de la placa S3CEV40.
+**
+**-----------------------------------------------------------------*/
+
+#ifndef KEYBOARD_H_
+#define KEYBOARD_H_
+
+int kb_scan(void);
+
+#endif
