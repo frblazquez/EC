83bacdb4a8af007aac6ecc8e0e8ac9a5ce5b6e12
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2019-03-24 14:35:44 +0100

    Practice 4 basic part 2 components

---
+++ Practise 4/parte2/utils.h
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------
+**
+**	  Francisco Javier Blázquez Martínez  ~  frblazqu@ucm.es 
+**      
+**	  Doble grado en Ingeniería informática - Matemáticas
+**	  Universidad Complutense de Madrid. Curso 2018/19
+**
+**    Estructura de Computadores. Facultad de Informática.
+**
+**    Descripción: 
+**	  Otras utilidades de la placa S3CEV40.
+**
+**-----------------------------------------------------------------*/
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void Delay( int time );
+
+#endif
